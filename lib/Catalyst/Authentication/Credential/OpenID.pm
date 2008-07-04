55465402830ccce2aaffb84d5affc2d5e6599687
Ashley Pond V <ashley@cpan.org>
2008-07-04 09:19:17 -0800

    import Catalyst-Authentication-Credential-OpenID 0.07 from CPAN
    git-cpan-module:   Catalyst-Authentication-Credential-OpenID
    git-cpan-version:  0.07
    git-cpan-authorid: ASHLEY
    git-cpan-file:     authors/id/A/AS/ASHLEY/Catalyst-Authentication-Credential-OpenID-0.07.tar.gz

--- lib/Catalyst/Authentication/Credential/OpenID.pm
+++ lib/Catalyst/Authentication/Credential/OpenID.pm
@@ -8,7 +8,7 @@ BEGIN {
     __PACKAGE__->mk_accessors(qw/ _config realm debug secret /);
 }
 
-our $VERSION = "0.06";
+our $VERSION = "0.07";
 
 use Net::OpenID::Consumer;
 use Catalyst::Exception ();
@@ -128,7 +128,7 @@ Catalyst::Authentication::Credential::OpenID - OpenID credential for Catalyst::P
 
 =head1 VERSION
 
-0.06
+0.07
 
 =head1 SYNOPSIS
 
